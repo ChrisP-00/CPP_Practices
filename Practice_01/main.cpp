a50816335129dfec6f6aba109032b03edfba2f79
Santa <jspark4151@gmail.com>
2023-12-30 17:57:46 +0900

    Test
    Test

---
+++ Practice_01/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, this is practice 1" << endl;
+
+    return 0;
+}
--- Practice_02/main.cpp
+++ Practice_02/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, this is practice 2" << endl;
 
     return 0;
 }
