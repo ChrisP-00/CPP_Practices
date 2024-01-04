582890e599bcdcb2dbfffe8b4ded5091d9f13b05
ChrisP-00 <jspark4151@gmail.com>
2024-01-04 22:08:22 +0900

    Feat: solve third practice

---
+++ Chapter01/Practice_03/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void printMice()
+{
+    cout << "Three blind mice" << endl;
+}
+
+void printHow()
+{
+    cout << "See how they run" << endl;
+}
+
+int main()
+{
+    printMice();
+    printMice();
+    printHow();
+    printHow();
+}
