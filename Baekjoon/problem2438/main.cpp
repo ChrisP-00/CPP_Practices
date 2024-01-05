30740303e187810200ad3741c9fe088dd92348da
ChrisP-00 <jspark4151@gmail.com>
2024-01-05 21:43:11 +0900

    Feat: solve baekjoon problem 2438

---
+++ Baekjoon/problem2438/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int stars;
+
+    cin >> stars;
+
+    for(int i = 0; i <= stars; ++i)
+    {
+        for(int j = 0; j < i; ++j)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
