d03e2a6c3991a8f95667da682e2dd90b67a80619
ChrisP-00 <jspark4151@gmail.com>
2024-01-09 18:47:18 +0900

    Feat: solve baekjoon problem 10813

---
+++ Baekjoon/problem10813/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int arr[101] = {0}; 
+
+int main()
+{
+    int n, m, b1, b2;
+
+    cin >> n >> m;
+
+
+
+    for(int i = 1; i <= n; ++i)
+    {
+        arr[i] = i;
+    }
+
+
+    for(int i = 0; i < m; ++i)
+    {
+        cin >> b1 >> b2; 
+
+        int temp; 
+
+        temp = arr[b1];
+        arr[b1] = arr[b2];
+        arr[b2] = temp;
+    }
+
+    for(int i = 1; i <= n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
