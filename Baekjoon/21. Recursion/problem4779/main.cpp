a6cc44b83afbfb726c78e0941fbda44a842799bd
ChrisP-00 <jspark4151@gmail.com>
2024-01-18 22:53:49 +0900

    solve baekjoon problem 4779

---
+++ Baekjoon/21. Recursion/problem4779/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#define fastTime ios::sync_withstdio(false), cin.tie(0)
+
+using namespace std; 
+
+int blank = 0;
+
+void cantor (int n)
+{
+    if(n == 0)
+    {
+        cout << '-';
+        return;
+    }
+    
+    cantor(n - 1);
+
+    blank = pow(3, n - 1);
+    for(int idx = 0; idx < blank; idx++)
+    {
+        cout << ' ';
+    }
+
+    cantor(n - 1);
+}
+
+int main()
+{
+    int n = 0; 
+
+    cin >> n; 
+
+    cantor(n);
+
+    return 0;
+}
