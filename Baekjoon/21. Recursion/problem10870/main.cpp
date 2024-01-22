e34e4364316e3ca872f791b3b8ac6319317e33d8
ChrisP-00 <jspark4151@gmail.com>
2024-01-22 22:00:12 +0900

    solve baekjoon problem 10870

---
+++ Baekjoon/21. Recursion/problem10870/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#define fastTime ios::sync_withstdio(false), cin.tie(0);
+
+
+
+int fibonacciNumber(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+
+    if(n == 0)
+    {
+        return 0;
+    }
+
+    return fibonacciNumber(n - 1) + fibonacciNumber(n - 2);
+}
+
+
+
+int main()
+{
+    int n = 0;
+
+    cin >> n;
+
+    cout << fibonacciNumber(n);
+}
