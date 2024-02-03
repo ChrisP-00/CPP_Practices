a3db3a8f8a52505d81ace9da09840d85a4b3792d
ChrisP-00 <jspark4151@gmail.com>
2024-02-03 21:50:12 +0900

    add exercise code

---
+++ C++ Primer Plus/Chapter03/Lisiting03.10/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+    float hats, heads; 
+
+    cin >> hats >> heads; 
+
+
+    cout << "hats + heads = " << hats + heads << endl;
+    cout << "hats - heads = " << hats - heads << endl;
+
+    return 0;
+}
