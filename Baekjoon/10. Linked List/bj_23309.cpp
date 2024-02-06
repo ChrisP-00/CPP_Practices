78c481027170f95f8720a20f9f19369ebc483dc1
ChrisP-00 <jspark4151@gmail.com>
2024-02-06 23:49:18 +0900

    Solve baekjoon problem 23309

--- Baekjoon/10. Linked List/bj_23309.cpp
+++ Baekjoon/10. Linked List/bj_23309.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 
 using namespace std;
+
+struct node
+{
+    int prev;
+    int next;
+};
+
 
 int main()
 {
     ios::sync_with_stdio(false), cin.tie(0);
 
-    list<int> myList;
+    /*list<int> myList;
     list<int>::iterator iter = myList.begin();
 
     string command = "";
@@ -104,6 +111,104 @@ int main()
         }
     }
 
-    myList.clear();
+    myList.clear();*/
+
+    vector<node> myList(1000001);
+    vector<int> stations; 
+
+    int stationNumber, station, constructionCount = 0;
+    int head, tail, prevNum = 0;
+
+
+    string command = "";
+
+    cin >> stationNumber >> constructionCount;
+
+    for(int idx = 0; idx < stationNumber; idx++)
+    {
+        cin >> station;
+
+        if(idx == 0)
+        {
+            head = station;
+        }
+        else if(idx == stationNumber - 1)
+        {
+            tail = station;
+        }
+        
+        // set prev as previous station number 
+        myList[station].prev = prevNum;
+        // leave next
+        myList[station].next = 0;
+
+        // set prev station's next as current number 
+        myList[prevNum].next = station;
+        // set prevNumber as station 
+        prevNum = station;
+    }
+
+    // connect head and tail 
+    myList[head].prev = tail;
+    myList[tail].next = head;
+
+    while(constructionCount--)
+    {
+        cin >> command;
+        cin >> stationNumber;
+        
+        if(command == "BN") // print next station number and add new station after
+        {
+            // new station 
+            cin >> station;
+
+            cout << myList[stationNumber].next << '\n';
+
+            // new station's next is current staion's next 
+            myList[station].next = myList[stationNumber].next;
+            // new stations' prev is current station
+            myList[station].prev = stationNumber;
+            // current station's next -> prev is new station 
+            myList[myList[stationNumber].next].prev = station;
+            // current station's next is new station
+            myList[stationNumber].next = station;
+
+        }
+        else if(command == "BP") // print previous station number and add new station front 
+        {
+            // new station 
+            cin >> station;
+
+            cout << myList[stationNumber].prev << '\n';
+            
+            // new station's next is current station
+            myList[station].next = stationNumber;
+            // new station's prev is current station's prev 
+            myList[station].prev = myList[stationNumber].prev;
+            // current station's prev -> next is new station 
+            myList[myList[stationNumber].prev].next = station;
+            // current station's prev is new station
+            myList[stationNumber].prev = station;
+        }
+        else if(command == "CN") // delete next station and print the deleted station number 
+        {
+            cout << myList[stationNumber].next << '\n';
+
+            // next -> next -> station's prev = current station
+            myList[myList[myList[stationNumber].next].next].prev = stationNumber;
+            // current station's next = next -> next station
+            myList[stationNumber].next = myList[myList[stationNumber].next].next;
+        }
+        else if(command == "CP") // delete previous station and print the deleted station number 
+        {
+            cout << myList[stationNumber].prev << '\n';
+            
+            // prev -> prev -> station's next = current station
+            myList[myList[myList[stationNumber].prev].prev].next = stationNumber;
+            // current station's prev = prev -> prev station
+            myList[stationNumber].prev = myList[myList[stationNumber].prev].prev;
+
+        }
+    }
     return 0;
 }
