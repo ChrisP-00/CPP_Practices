0bc4a8d7e77928fb334b3bf910735d06df0321a0
ChrisP-00 <jspark4151@gmail.com>
2024-02-08 20:30:23 +0900

    Refactor: bj_23309.cpp

--- Baekjoon/10. Linked List/bj_23309.cpp
+++ Baekjoon/10. Linked List/bj_23309.cpp
@@ -10,108 +10,9 @@ struct node
     int next;
 };
 
-
 int main()
 {
     ios::sync_with_stdio(false), cin.tie(0);
-
-    /*list<int> myList;
-    list<int>::iterator iter = myList.begin();
-
-    string command = "";
-    int stationNumber, station, constructionCount = 0;
-
-    cin >> stationNumber >> constructionCount;
-
-    while(stationNumber--)
-    {
-        cin >> station;
-
-        myList.push_back(station);
-    }
-
-    while(constructionCount--)
-    {
-        cin >> command;
-        cin >> stationNumber;
-
-        while(*iter != stationNumber)
-        {
-            if(iter == myList.end())
-            {
-                iter = myList.begin();
-            }
-            else
-            {
-                iter++;
-            }
-        }
-        
-        if(command == "BN") // print next station number and add new station front
-        {
-            cin >> station;
-
-            if(*iter == myList.back())
-            {
-                iter = myList.begin();
-            }
-            else
-            {
-                ++iter;
-            }
-
-            cout << *iter << '\n';
-            iter = myList.insert(iter, station);
-
-        }
-        else if(command == "BP") // print previous station number and add new station after 
-        {
-            cin >> station;
-
-            if(*iter == myList.front())
-            {
-                iter = myList.end();
-            }
-            else
-            {
-                --iter;
-            }
-
-            cout << *iter << '\n';
-            ++iter;
-            iter = myList.insert(iter, station);
-        }
-        else if(command == "CP") // delete next station and print the deleted station number 
-        {
-            if(*iter == myList.front())
-            {
-                iter = myList.end();
-            }
-            else
-            {
-                --iter;
-            }
-
-            cout << *iter << '\n';
-            iter = myList.erase(iter);
-        }
-        else if(command == "CN") // delete previous station and print the deleted station number 
-        {
-            if(*iter == myList.back())
-            {
-                iter = myList.begin();
-            }
-            else
-            {
-                ++iter;
-            }
-
-            cout << *iter << '\n';
-            iter = myList.erase(iter);
-        }
-    }
-
-    myList.clear();*/
 
     vector<node> myList(1000001);
     vector<int> stations; 
@@ -207,7 +108,6 @@ int main()
             myList[myList[myList[stationNumber].prev].prev].next = stationNumber;
             // current station's prev = prev -> prev station
             myList[stationNumber].prev = myList[myList[stationNumber].prev].prev;
-
         }
     }
     return 0;
