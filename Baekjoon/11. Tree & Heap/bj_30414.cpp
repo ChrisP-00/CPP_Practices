74f551f22b8b9c4ab393ffb356f1294a32e829f1
ChrisP-00 <jspark4151@gmail.com>
2024-02-29 20:51:07 +0900

    working on baekjoon problem 30414

--- Baekjoon/11. Tree & Heap/bj_30414.cpp
+++ Baekjoon/11. Tree & Heap/bj_30414.cpp
@@ -5,46 +5,58 @@ using namespace std;
 
 struct myNode
 {
-    myNode* children [100001];
-
+    vector<int> children;
     int height;
 
+    myNode() : children(100001), height(0) {}
 
-    void InsertNode()
+
+    void printData(int root)
+    {
+        if(root == 0)
+        {
+            return;
+        }
+
+        cout << height << '\n';
+
+        printData(children[root]);
+    }
 };
 
 
-vector<myNode> myTree(100001);
+vector<myNode> myTree (100001);
 
 
 
 
 int main()
 {
-    vector<int> mointainHeight ()
     int n, p, input1, input2 = 0;
 
     cin >> n >> p;
 
-    for(int idx = 1; idx < n; idx++)
+    for(int idx = 1; idx < n + 1; idx++)
     {
         cin >> input1;
         myTree[idx].height = input1;
     }
 
-    for(int idx = 1; idx < n; idx++)
+    for(int idx = 1; idx < n + 1; idx++)
     {
         cin >> input1;
         myTree[idx].height -= input1;
     }
 
-    for(int idx = 0; idx < n - 1; idx++)
+    for(int idx = 1; idx < n; idx++)
     {
         cin >> input1 >> input2;
-        myTree[input1].children = 
+
+        myTree[input1].children.push_back(input2);
+        myTree[input2].children.push_back(input1);
     }
 
-
+    
 
 
     return 0;
