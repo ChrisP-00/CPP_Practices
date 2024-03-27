25380c01109a9329753ab1424836e8e5477ddb6d
ChrisP <jspark4151@gmail.com>
2024-03-27 13:59:20 +0900

    working on baekjoon problem 30960

---
+++ Baekjoon/15. Greedy/bj_30960.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+
+using namespace std; 
+
+vector<int> people;
+vector<int> group;
+
+int main()
+{
+    int n, m, result = 0;
+
+    cin >> n;
+
+    m = n / 2;
+
+    people.assign(n,0);
+    group.assign(m,0);
+
+    for(int idx = 0; idx < n; idx++)
+    {
+        cin >> people[idx];
+    }
+
+    sort(people.begin(), people.end());
+
+    for(int idx = 0; idx < n-1; idx++)
+    {
+        group[idx] = people[idx+1] - people[idx];
+        cout << "idx" << idx << " shy: " << people[idx+1] - people[idx] << '\n';
+    }
+
+    sort(group.begin(), group.end());
+
+    for(int idx = 1; idx < m; idx++)
+    {
+        cout << "group: " << group[idx] << '\n';
+        result += group[idx];
+    }
+
+    cout << result;
+
+    return 0;
+}
